169814aa10a49a317b7c36b25e08233ba5f422c0
¨lucia_ab¨ <luabramzon@gmail.com>
2021-11-29 17:36:01 -0300

    subida

---
+++ pago/pago.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("side-menu").style.width = "50%"; // QUE CUBRA LA MITAD DE LA PANTALLA
+  }
+  
+  function closeNav() {
+    document.getElementById("side-menu").style.width = "0";
+  }
---
+++ tienda/tienda.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("side-menu").style.width = "50%"; // QUE CUBRA LA MITAD DE LA PANTALLA
+  }
+  
+  function closeNav() {
+    document.getElementById("side-menu").style.width = "0";
+  }
+
